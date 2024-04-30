38ba5fd382b41768546c7e2d66445f0ab78b4dfd
SantiagoNeira21 <santigla047@gmail.com>
2024-04-30 14:13:20 -0500

    MiEvento

---
+++ src/Screens/Home.js
@@ -0,0 +1,21 @@
+import React from 'react';
+function Home() {
+   
+    return (
+        <div  className="Home">
+          <h1 className="Home-h1">
+            Bienvenido a nuestra página de reservas de eventos
+            </h1>
+
+          <p className="Home-p">
+
+            ¡Encuentra y reserva los mejores eventos cerca de ti!
+            
+            </p>
+          
+        </div>
+      );
+}
+
+export default Home;
+
