aff167d9cbc308f98e04394f2a04ee4c26cf3879
Santiago Santafe <andressasi@unisabana.edu.co>
2024-05-17 21:50:11 -0500

    Update URL in saveUsuarios.js for user registration

--- src/Peticiones/saveUsuarios.js
+++ src/Peticiones/saveUsuarios.js
@@ -1,6 +1,6 @@
 
 export const saveUsuarios = async (userData) => {
-  const url = 'http://localhost:8080/api/v1/cliente/usuarios/resgistro'; // Reemplaza con la URL correcta de tu backend
+  const url = 'http://localhost:8080/api/v1/cliente/usuario/registro'; // Reemplaza con la URL correcta de tu backend
 
   try {
     const response = await fetch(url, {
@@ -13,11 +13,12 @@ export const saveUsuarios = async (userData) => {
 
     if (!response.ok) {
       throw new Error(`Error al registrar el usuario: ${response.status}`);
+    } else{
+      console.log('Usuario registrado exitosamente');
+      return true; 
     }
-
     // Aquí puedes procesar la respuesta del backend si es necesario
-    console.log('Usuario registrado exitosamente');
-    return true; // Retorna true para indicar que el registro fue exitoso
+    // Retorna true para indicar que el registro fue exitoso
   } catch (error) {
     console.error('Error al registrar el usuario:', error);
     return false; // Retorna false para indicar que hubo un error
