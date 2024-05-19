ff7770621a619935be6e510ca5434c37c3de8aab
Andres Santiago Santafe Silva <andressasi@unisabana.edu.co>
2024-05-18 22:27:46 -0500

    Docker

---
+++ deploy.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Construir la imagen de Docker
+docker build -t mievento -f Dockerfile.react .
+
+# Detener y eliminar el contenedor antiguo
+docker stop mievento
+docker rm mievento
+
+# Iniciar un nuevo contenedor con la imagen recién construida
+docker run -d --name mievento -p 3000:3000 mievento
